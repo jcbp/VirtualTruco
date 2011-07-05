b35896b72036aa139930dcead6a729ddff0f5dc6
jcbp <jcbp.jcbp@gmail.com>
2011-07-04 23:48:32 -0300

    Se agrego la interfaz de la api del player

---
+++ playerAPI.js
@@ -0,0 +1,58 @@
+/*
+ * Utils
+ */
+var Utils = function () {
+	this.getLastElement = function (elements) {
+		return elements[elements.length - 1];
+	}
+}
+/*
+ * API
+ */
+var PlayerAPI = new function () {
+	
+	this.ChallengeFactory = new function () {
+		this.createEnvido = function () {
+			return new Server.Action(Server.ActionType.PostFirstPartChallenge, Server.Messages.Envido);
+		}
+		this.createRealEnvido = function () {
+			return new Server.Action(Server.ActionType.PostFirstPartChallenge, Server.Messages.RealEnvido);
+		}
+		this.createTruco = function () {
+			return new Server.Action(Server.ActionType.PostSecondPartChallenge, Server.Messages.Truco);
+		}
+	}
+	
+	this.GameDataSet = function (moves) {
+		this.getOpponentMoves = function () {
+
+		}
+		this.getOwnMoves = function () {
+			
+		}
+		this.getOpponentCardsPlayed = function () {
+
+		}
+		this.getOwnCardsPlayed = function () {
+
+		}
+		this.getLastMovePlayer = function () {
+			
+		}
+	}
+	
+	this.CardManager = function (cards) {
+		// el mas alto
+		this.calculateEnvido = function () {
+			
+		}
+		// peso de la carta
+		this.getCardWeight = function (card) {
+	
+		}
+		// ordenadas por peso ascendente
+		this.getWinnerCards = function (opponentCard) {
+			
+		}
+	}
+}
