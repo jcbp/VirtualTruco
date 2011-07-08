1ce2f0ef3e7b8f74e2163feab92f495c7bfbc62a
ferc <facarril@gmail.com>
2011-07-07 22:20:07 -0700

    Se implemento el método para calcular el envido.

--- playerAPI.js
+++ playerAPI.js
@@ -58,17 +58,130 @@ var CommonAPI = new function () {
 	}
 	
 	this.CardPacketManager = function (cards) {
-		// el mas alto
-		this.calculateEnvido = function () {
-			
+		
+		/*
+		 * Se define el peso de las cartas	
+		 */
+		var _cardsWeight = {
+			Cup: {
+				1: 7,
+				2: 6,
+				3: 5,
+				4: 14,
+				5: 13,
+				6: 12,
+				7: 11,
+				10: 10,
+				11: 9,
+				12: 8
+			},
+			Coin:  {
+				1: 7,
+				2: 6,
+				3: 5,
+				4: 14,
+				5: 13,
+				6: 12,
+				7: 4,
+				10: 10,
+				11: 9,
+				12: 8
+			},
+			Club:  {
+				1: 2,
+				2: 6,
+				3: 5,
+				4: 14,
+				5: 13,
+				6: 12,
+				7: 11,
+				10: 10,
+				11: 9,
+				12: 8
+			},
+			Sword:  {
+				1: 1,
+				2: 6,
+				3: 5,
+				4: 14,
+				5: 13,
+				6: 12,
+				7: 3,
+				10: 10,
+				11: 9,
+				12: 8
+			}
+		};
+		
+		/*
+		 * Se definen las enumeraciones de los posibles resultados de comparar el peso de dos cartas
+		 */
+		var CompareWeightType = this.CompareWeightType = {
+			Less: -1,
+			Equal: 0,
+			Higher: 1
+		};
+
+		/*
+		 * Retorna el valor del envido de una carta
+		 */
+		var getEnvidoValue = function (card) {
+			return card.value <= 7 ? card.value : 0;
 		}
-		// peso de la carta
-		this.getCardWeight = function (card) {
-	
+
+		/*
+		 * Retorna el envido de dos cartas
+		 */
+		var calculatePartialEnvido = this.calculatePartialEnvido = function (firstCard, secondCard) {
+			var envido;
+			if(firstCard.suit == secondCard.suit) {
+				envido = 20 + getEnvidoValue(firstCard) + getEnvidoValue(secondCard);
+			}
+			else {
+				envido = Math.max(getEnvidoValue(firstCard), getEnvidoValue(secondCard));
+			}
+			return envido;
 		}
-		// ordenadas por peso ascendente
-		this.getWinnerCards = function (opponentCard) {
-			
+		
+		/*
+		 * Retorna el envido mas alto posible evaluando todas las cartas de la mano
+		 */
+		 this.calculateEnvido = function() {
+			var envido = 0;
+			var i, j;
+			for(i = 0; i < cards.length; i++) {
+				for(j = i+1; j < cards.length; j++) {
+					envido = Math.max(envido, calculatePartialEnvido(cards[i], cards[j]));
+				}
+			}
+			return envido;
+		}
+
+		/*
+		 * Retorna el peso de la carta
+		 */
+		var getCardWeight = this.getCardWeight = function (card) {
+			return _cardsWeight[card.suit] && _cardsWeight[card.suit][card.value];
+		}
+		
+		/*
+		 * Retorna si la primer carta es mayor, menor o igual que la segunda
+		 */
+		this.compareWeight = function (firstCard, secondCard) {
+			var firstWeight = getCardWeight(firstCard);
+			var secondWeight = getCardWeight(secondCard);
+			return firstWeight < secondWeight ? CompareWeightType.Higher : firstWeight > secondWeight ? CompareWeightType.Less : CompareWeightType.Equal;
+		}
+
+		/*
+		 * Retorna las cartas ordenadas por peso en orden ascendente
+		 */
+		this.getWinnerCards = function () {
+			var orderedCards = [].concat(cards);
+			orderedCards.sort(function(firstCard, secondCard) {
+				return getCardWeight(firstCard) - getCardWeight(secondCard);
+			});
+			return orderedCards;
 		}
 	}
 }
