784d37c0e539c117bc8966a67b38200e5f341ccc
Ojes <ojesotrebla@yahoo.com>
2011-07-10 18:54:55 -0700

    Se agrega un nuevo template de manejo de jugadas para los players

--- jorgePlayer.js
+++ jorgePlayer.js
@@ -2,6 +2,14 @@
  * IA of Jorge
  */
 var Player1 = function () {
+var actionMove = [CommonAPI.ENVIDO,
+CommonAPI.TRUCO,
+CommonAPI.PLAY_CARD,
+CommonAPI.QUIERO,
+CommonAPI.PLAY_CARD,
+CommonAPI.PLAY_CARD
+];
+
 	var _cardSet = [];
 	var _scorePoints = 0;
 	var _utils = new Utils();
@@ -31,20 +39,39 @@ var Player1 = function () {
 		
 		
 		var action;
-	
-		i++;
-		switch(i) {
-			case 1:
-				action = CommonAPI.ActionFactory.createEnvido(); 
+                switch(actionMove[i++]) {
+			case CommonAPI.ENVIDO:
+				action = CommonAPI.ActionFactory.createEnvido();
 				break;
-			case 2:
+			case CommonAPI.REAL_ENVIDO:
 				action = CommonAPI.ActionFactory.createRealEnvido(); 
 				break;
-			case 3:
-				action = CommonAPI.ActionFactory.createPostScore(30);
+			case CommonAPI.FALTA_ENVIDO:
+				action = CommonAPI.ActionFactory.createFaltaEnvido();
 				break;
-			case 6:
-				action = CommonAPI.ActionFactory.createQuiero();
+			case CommonAPI.SON_BUENAS:
+				action = CommonAPI.ActionFactory.createSonBuenas();
+				break;
+			case  CommonAPI.TRUCO:
+				action = CommonAPI.ActionFactory.createTruco();
+				break;
+			case  CommonAPI.RE_TRUCO:
+				action = CommonAPI.ActionFactory.createReTruco();
+				break;
+			case  CommonAPI.VALE_CAUTRO:
+				action = CommonAPI.ActionFactory.createValeCuatro();
+				break;
+			case  CommonAPI.QUIERO:
+				action = CommonAPI.ActionFactory.createQuiero(); 
+				break;
+			case  CommonAPI.NO_QUIERO:
+				action = CommonAPI.ActionFactory.createNoQuiero();
+				break;
+			case CommonAPI.POST_SCORE:
+				action = CommonAPI.ActionFactory.createPostScore(28);
+				break;
+			case CommonAPI.GO_TO_DECK:
+				action = CommonAPI.ActionFactory.createGoingToDeck();
 				break;
 			default:
 				action = CommonAPI.ActionFactory.createPlayCard(_cardSet.pop());
