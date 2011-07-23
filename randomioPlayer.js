1586ea5ee1ffc06fc34d3e8a566c1d6a5df72722
jcbp <jcbp.jcbp@gmail.com>
2011-07-23 18:36:16 -0300

    se comento el log del stack de jugadas

--- randomioPlayer.js
+++ randomioPlayer.js
@@ -78,14 +78,14 @@ var RandomPlayer = function (name) {
 				break;
 		}
 		
-		for (var i=0; i < lastActions.length; i++) {
+		/* for (var i=0; i < lastActions.length; i++) {
 			Log.add({
 				actionType: lastActions[i].action.type,
 				actionMessage: (lastActions[i].action.message || {}).name,
 				actionCard: (lastActions[i].action.card || null) + "",
 				playerName: lastActions[i].player
 			});
-		};
+		}; */
 		Log.add({
 			Juega: playerName,
 			Message: action.message? action.message.name: action.card
